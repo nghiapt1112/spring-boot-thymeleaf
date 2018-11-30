9250756882af4618f53787630bdbaa04db4a752c
Ngo Manh Linh <linhnm@vnext.com.vn>
2018-11-30 09:33:11 +0700

    add file

---
+++ web-app/src/main/java/com/lyna/web/domain/postCourse/repository/PostCourseRepository.java
@@ -0,0 +1,9 @@
+package com.lyna.web.domain.postCourse.repository;
+
+import com.lyna.web.domain.postCourse.PostCourse;
+import com.lyna.web.domain.stores.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostCourseRepository extends JpaRepository<PostCourse, Long> {
+
+}
