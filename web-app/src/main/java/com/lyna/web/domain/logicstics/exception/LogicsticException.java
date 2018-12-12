b523fa7d9a0f457b929ae67123f254d930e402de
Ngo Manh Linh <linhnm@vnext.com.vn>
2018-12-12 10:52:48 +0700

    Fix bug display and convention

--- web-app/src/main/java/com/lyna/web/domain/logicstics/exception/LogicsticException.java
+++
@@ -1,9 +0,0 @@
-package com.lyna.web.domain.logicstics.exception;
-
-import com.lyna.commons.infrustructure.exception.DomainException;
-
-public class LogicsticException extends DomainException {
-    public LogicsticException(int errorCode, String message) {
-        super(Domain.User.code(), errorCode, message);
-    }
-}
--- web-app/src/main/java/com/lyna/web/domain/mpackage/exception/PackageException.java
+++
@@ -1,9 +0,0 @@
-package com.lyna.web.domain.mpackage.exception;
-
-import com.lyna.commons.infrustructure.exception.DomainException;
-
-public class PackageException extends DomainException {
-    public PackageException(int errorCode, String message) {
-        super(Domain.Package.code(), errorCode, message);
-    }
-}
