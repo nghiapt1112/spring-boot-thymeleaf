9469908f40c0ba077ac3b947df2d588fb1c8a6d5
Lee_Hieu <hieulh@vnext.com.vn>
2018-12-18 15:48:49 +0700

    format code

--- web-app/src/main/java/com/lyna/web/application/ProductController.java
+++ web-app/src/main/java/com/lyna/web/application/ProductController.java
@@ -14,7 +14,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -113,7 +119,6 @@ public class ProductController extends AbstractCustomController {
     @GetMapping(value = "/update/{productId}/{tenantId}")
     public String updateProduct(@PathVariable("productId") String productId, Model model,
                                 @PathVariable("tenantId") int tenantId, UsernamePasswordAuthenticationToken principal) {
-        User user = (User) principal.getPrincipal();
         Product product = productService.findOneByProductIdAndTenantId(productId, tenantId);
         model.addAttribute("product", product);
         return PRODUCT_EDIT_PAGE;
--- web-app/src/main/java/com/lyna/web/application/StoreController.java
+++ web-app/src/main/java/com/lyna/web/application/StoreController.java
@@ -18,7 +18,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
