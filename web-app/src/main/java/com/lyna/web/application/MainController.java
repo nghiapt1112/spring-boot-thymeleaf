03a6a5b24dc3c61ac0854d032dc617f6a0d032e6
Anh Quan <anhquan@Anhs-MacBook-Pro.local>
2018-12-18 09:22:55 +0700

    edit varriable name post and refactor code

--- web-app/src/main/java/com/lyna/web/application/MainController.java
+++ web-app/src/main/java/com/lyna/web/application/MainController.java
@@ -1,7 +1,6 @@
 package com.lyna.web.application;
 
 import com.lyna.commons.infrustructure.controller.AbstractCustomController;
-import com.lyna.commons.utils.DateTimeUtils;
 import com.lyna.web.domain.logicstics.LogisticRequestPage;
 import com.lyna.web.domain.logicstics.service.LogisticService;
 import com.lyna.web.domain.order.OrderRequestPage;
@@ -20,7 +19,9 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
-import static com.lyna.commons.utils.DateTimeUtils.*;
+import static com.lyna.commons.utils.DateTimeUtils.getCurrentDate;
+import static com.lyna.commons.utils.DateTimeUtils.fromNumber;
+import static com.lyna.commons.utils.DateTimeUtils.convertDateToString;
 import static com.lyna.web.domain.logicstics.LogisticRequestPage.END_DATE;
 import static com.lyna.web.domain.logicstics.LogisticRequestPage.POST_NAME;
 import static com.lyna.web.domain.logicstics.LogisticRequestPage.START_DATE;
@@ -47,14 +48,14 @@ public class MainController extends AbstractCustomController {
         User currentUser = (User) principal.getPrincipal();
         Date startDay = Objects.isNull(start) ? getCurrentDate() : fromNumber(start);
         Date endDay = Objects.isNull(end) ? getCurrentDate() : fromNumber(end);
-        String newPostName = (StringUtils.isEmpty(postName) || StringUtils.isEmpty(postName.trim())) ? null : postName;
+        String searchPostName = (StringUtils.isEmpty(postName) || StringUtils.isEmpty(postName.trim())) ? null : postName;
 
         LogisticRequestPage logisticQueryBuilder = new LogisticRequestPage();
         logisticQueryBuilder
                 .withTenantId(currentUser.getTenantId())
                 .addSearchField(START_DATE, startDay)
                 .addSearchField(END_DATE, endDay)
-                .addSearchField(POST_NAME, newPostName)
+                .addSearchField(POST_NAME, searchPostName)
                 .build();
 
         Map<String, Object> logisticData = logisticService.findLogisticsView(currentUser.getTenantId(), logisticQueryBuilder);
@@ -64,7 +65,7 @@ public class MainController extends AbstractCustomController {
                 .withTenantId(currentUser.getTenantId())
                 .addSearchField(START_DATE, startDay)
                 .addSearchField(END_DATE, endDay)
-                .addSearchField(POST_NAME, newPostName)
+                .addSearchField(POST_NAME, searchPostName)
                 .build();
 
         model.addAttribute(LOGISTIC_DATA, logisticData.get(LOGISTIC_DATA));
@@ -72,7 +73,7 @@ public class MainController extends AbstractCustomController {
         model.addAttribute("orderData", orderService.findOrderViews(currentUser.getTenantId(), orderQueryBuilder));
         model.addAttribute("dateStart", convertDateToString(startDay));
         model.addAttribute("dateEnd", convertDateToString(endDay));
-        model.addAttribute("postName" , newPostName);
+        model.addAttribute("postName" , searchPostName);
         return "main/mainMenu";
     }
 
