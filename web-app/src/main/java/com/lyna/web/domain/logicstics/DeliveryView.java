95a79d54fd79ab4731f059b50918c15f30045939
nghia.pham <nghiapt1112@gmail.com>
2018-12-21 13:27:12 +0700

    [task-#14678]
    Show difference color for each type of data:
     - Logistic: no color
     - Delivery override: green
     - Delivery original: orange

--- web-app/src/main/java/com/lyna/web/domain/logicstics/DeliveryView.java
+++ web-app/src/main/java/com/lyna/web/domain/logicstics/DeliveryView.java
@@ -7,54 +7,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.Objects;
 
 @Entity
 @Table(name = "v_delivery")
 @Data
 @NoArgsConstructor
-public class DeliveryView {
+public class DeliveryView extends MainMenuView {
     @Id
-    @Column(name = "order_id")
-    private String orderId;
-
-    @Column
-    private int tenantId;
-
     @Column
     private String delivery_id;
-
-    @Column
-    private Date orderDate;
-
-    @Column
-    private String storeName;
-
-    @Column
-    private String post;
-
-    @Column
-    private BigDecimal amount;
-
-    @Column
-    private BigDecimal price;
-
-    @Column
-    private String packageId;
-
-    @Column
-    private String packageName;
-
-    @Column(name = "full_load_weight")
-    private BigDecimal totalWeight;
-
-    @Column(name = "full_load_capacity")
-    private BigDecimal totalCapacity;
-
-    @Column
-    private String course;
 
     public boolean isPackageNameNonNull() {
         return Objects.nonNull(packageName);
