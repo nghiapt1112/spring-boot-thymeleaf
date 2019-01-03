1301decaa9775ace1fc694235ffecce278854d6c
lehonghieu1990 <44346838+lehonghieu1990@users.noreply.github.com>
2019-01-03 10:57:56 +0700

    16091995

--- web-app/src/main/resources/static/js/listUser.js
+++ web-app/src/main/resources/static/js/listUser.js
@@ -1,4 +1,33 @@
 $(document).ready(function () {
     deleteTable("user","#listUser");
+
+    var table = $("#listUser").DataTable({
+        'paging': true,
+        'scrollX': true,
+        'scrollCollapse': true,
+        'fixedColumns':   {
+            'leftColumns': 3
+        },
+        'lengthChange': true,
+        'searching': true,
+        'ordering': true,
+        'info': true,
+        'autoWidth': true,
+        'order': [2 , 'asc'],
+        "columnDefs": [{'orderable': false, 'targets': [0]}],
+        "language": {
+            "lengthMenu": "  _MENU_ 件を表示",
+            "zeroRecords": "該当データが存在しません。",
+            "info": "_TOTAL_ 件中 _START_ ~ _END_  件を表示",
+            "infoEmpty": "該当データが存在しません。",
+            "infoFiltered": "(件を表示 _MAX_ total records)",
+            "search": "検索:",
+            "paginate": {
+                "previous": " 前へ ",
+                "next": "  次へ "
+            }
+        }
+
+    });
 });
-dataTable("#listUser");
+
