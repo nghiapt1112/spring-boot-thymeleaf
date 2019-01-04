43871bd33f85415f6074a7ec1918fefeb792c3c8
lehonghieu1990 <44346838+lehonghieu1990@users.noreply.github.com>
2019-01-04 16:41:30 +0700

    xuất excel tất các màn hình list, fix cuộn ngang listUser 3:22 4/1/2019

--- web-app/src/main/java/com/lyna/web/domain/AI/Data.java
+++ web-app/src/main/java/com/lyna/web/domain/AI/Data.java
@@ -1,50 +1,12 @@
 package com.lyna.web.domain.AI;
 
 import com.lyna.commons.infrustructure.object.AbstractObject;
-import com.lyna.web.domain.mpackage.Package;
-import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @lombok.Data
 public abstract class Data extends AbstractObject {
-    public List<Integer> inputItemNums;
-    public List<Integer> outputItemNums;
+    protected List<Integer> inputItemNums;
+    protected List<Integer> outputItemNums;
 }
 
-@NoArgsConstructor
-class TrainingData extends Data {
-
-}
-
-@NoArgsConstructor
-class UnknownData extends Data {
-    private String key;
-
-    public UnknownData(String key, List<Integer> el) {
-        super();
-        this.key = key;
-        this.inputItemNums = el;
-    }
-
-    public String getOrderId() {
-        return key;
-    }
-
-    /**
-     * create a map: packageId <-> amount
-     *
-     */
-    public Map<String, Integer> toMap(List<Package> pkgsInTenant) {
-        Map<String, Integer> amountByPgkId = new HashMap<>();
-        for (int i = 0; i < this.outputItemNums.size(); i++) {
-            String k = pkgsInTenant.get(i).getPackageId();
-            Integer v = this.outputItemNums.get(i); // nhung cai bang 0 thi khong can cho vao nhi?
-            amountByPgkId.put(k, v);
-        }
-        return amountByPgkId;
-    }
-
-}
---
+++ web-app/src/main/java/com/lyna/web/domain/AI/TrainingData.java
@@ -0,0 +1,16 @@
+package com.lyna.web.domain.AI;
+
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+
+@NoArgsConstructor
+public class TrainingData extends Data {
+
+    public static TrainingData defaultTrainingData() {
+        TrainingData tdt = new TrainingData();
+        tdt.setInputItemNums(Arrays.asList(2,2,2,2));
+        tdt.setOutputItemNums(Arrays.asList(3,3,3,3));
+        return tdt;
+    }
+}
