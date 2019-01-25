1243b882558ddbd19c8f08953a8d6d9a0dbe7f15
anhquan12 <quangun2412@gmail.com>
2019-01-25 14:49:24 +0700

    fix display principal name in session

--- web-app/src/main/java/com/lyna/web/application/UserController.java
+++ web-app/src/main/java/com/lyna/web/application/UserController.java
@@ -166,7 +166,7 @@ public class UserController extends AbstractCustomController {
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-
+        currentUser.setName(aggregate.getName());
         userService.update(currentUser, aggregate);
         DataUtils.putMapData(Constants.ENTITY_STATUS.UPDATED, currentUser.getId());
         return REDIRECT_TO_USER_LIST_PAGE;
